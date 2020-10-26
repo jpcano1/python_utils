05abff6639a21adfced1e1e21c4df450b66bc4a3
Juan Pablo <jp.cano@uniandes.edu.co>
2020-10-26 10:55:23 -0500

    Autoencoder mejorado

--- ISIS_4825/ML/Taller_12/torch_utils.py
+++ ISIS_4825/ML/Taller_12/torch_utils.py
@@ -1,5 +1,5 @@
 from torch import nn
-import torch
+import numpy as np
 
 """
 Autoencoder Creator
@@ -19,8 +19,9 @@ def get_upsample(scale_factor=2, mode="bilinear"):
         x = nn.Upsample(scale_factor=scale_factor, mode=mode)
     return x
 
-class Conv2dBlock(nn.Module):
+class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super(ConvBlock, self).__init__()
         kernel_size = kwargs.get("kernel_size") or 3
         stride = kwargs.get("stride") or 1
         padding = kwargs.get("padding") or 1
@@ -36,7 +37,7 @@ class Conv2dBlock(nn.Module):
                                 padding_mode=padding_mode, 
                                 padding=padding)
         layers.append(conv2d_layer)
-        if torch.rand() < bn:
+        if np.random.rand() < bn:
             bn_layer = nn.BatchNorm2d(out_channels)
             layers.append(bn_layer)
 
@@ -45,6 +46,19 @@ class Conv2dBlock(nn.Module):
 
     def forward(self, x):
         return self.conv_block(x)
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, scale_factor=2, mode="bilinear", *args, **kwargs):
+        super(UpsampleBlock, self).__init__()
+        if mode != "nearest":
+            self.upsample_layer = nn.Upsample(scale_factor=scale_factor, 
+                                              mode=mode, align_corners=True)
+        else:
+            self.upsample_layer = nn.Upsample(scale_factor=scale_factor,
+                                              mode=mode)
+            
+    def forward(self, x):
+        return self.upsample_layer(x)
         
 class Encoder(nn.Module):
     def __init__(self, in_channels, init_filters, depth, **kwargs):
@@ -53,13 +67,12 @@ class Encoder(nn.Module):
         pool_size = kwargs.get("pool_size") or 2
         pool_stride = kwargs.get("pool_stride") or 2
 
+        bn = kwargs.get("bn") or 0
+
         jump = kwargs.get("jump") or 1
 
-        init_layer = create_convolution_block(in_channels, init_filters)
+        init_layer = ConvBlock(in_channels, init_filters)
         layers.append(init_layer)
-
-        layer = kwargs.get("activation") or nn.LeakyReLU(0.2)
-        layers.append(layer)
 
         current_filters = init_filters
         
@@ -68,12 +81,8 @@ class Encoder(nn.Module):
 
             for _ in range(jump - 1):
                 # Convolution Block
-                layer = create_convolution_block(current_filters,
-                                                 current_filters * 2)
-                layers.append(layer)
-
-                # Activation Block
-                layer = nn.LeakyReLU(0.2) or kwargs.get("activation")
+                layer = ConvBlock(current_filters,
+                                  current_filters * 2, bn=bn)
                 layers.append(layer)
 
                 current_filters *= 2
@@ -83,12 +92,8 @@ class Encoder(nn.Module):
             layers.append(layer)
 
             # Convolution Block
-            layer = create_convolution_block(current_filters, 
-                                             current_filters * 2)
-            layers.append(layer)
-
-            # Activation Block
-            layer = nn.LeakyReLU(0.2) or kwargs.get("activation")
+            layer = ConvBlock(current_filters,
+                              current_filters * 2, bn=bn)
             layers.append(layer)
 
             current_filters *= 2
@@ -106,35 +111,30 @@ class Decoder(nn.Module):
         scale_factor = kwargs.get("scale_factor") or 2 
         mode = kwargs.get("mode") or "bilinear"
 
+        bn = kwargs.get("bn") or 0
+
         jump = kwargs.get("jump") or 1
 
         current_filters = init_filters
 
         for _ in range(depth - 1):    
             for _ in range(jump - 1):
-                layer = create_convolution_block(current_filters,
-                                                 current_filters // 2)
-                layers.append(layer)
-
-                layer = kwargs.get("activation") or nn.LeakyReLU(0.2)
+                layer = ConvBlock(current_filters,
+                                  current_filters // 2, bn=bn)
                 layers.append(layer)
 
                 current_filters //= 2
 
-            layer = get_upsample(scale_factor=scale_factor, mode=mode)
+            layer = UpsampleBlock(scale_factor=scale_factor, mode=mode)
             layers.append(layer)
-            layer = create_convolution_block(current_filters,
-                                             current_filters // 2)
-            layers.append(layer)
-            layer = kwargs.get("activation") or nn.LeakyReLU(0.2)
+            layer = ConvBlock(current_filters,
+                              current_filters // 2)
             layers.append(layer)
 
             current_filters //= 2
 
-        layer = create_convolution_block(current_filters, 
-                                         out_channels)
-        layers.append(layer)
-        layer = nn.Sigmoid()
+        layer = ConvBlock(current_filters, out_channels, 
+                          activation=nn.Sigmoid(), bn=bn)
         layers.append(layer)
         self.decoder = nn.Sequential(*layers)
 
