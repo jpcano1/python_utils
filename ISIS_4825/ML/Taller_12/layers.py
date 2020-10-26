49510db37085716e6f5df597a4119825b41ee802
Juan Pablo <jp.cano@uniandes.edu.co>
2020-10-26 18:01:51 -0500

    Arreglos FInales

---
+++ ISIS_4825/ML/Taller_12/layers.py
@@ -0,0 +1,140 @@
+import torch
+from torch import nn
+
+import numpy as np
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, padding=1, *args, **kwargs):
+        super(ConvBlock, self).__init__()
+        kernel_size = kwargs.get("kernel_size") or 3
+        stride = kwargs.get("stride") or 1
+
+        padding_mode = kwargs.get("padding_mode") or "zeros"
+        activation = kwargs.get("activation") or nn.LeakyReLU(0.2)
+        bn = kwargs.get("bn") or 0
+
+        layers = []
+        conv2d_layer = nn.Conv2d(in_channels=in_channels, 
+                                out_channels=out_channels, 
+                                kernel_size=kernel_size, 
+                                stride=stride, 
+                                padding_mode=padding_mode, 
+                                padding=padding)
+        layers.append(conv2d_layer)
+        if np.random.rand() < bn:
+            bn_layer = nn.BatchNorm2d(out_channels)
+            layers.append(bn_layer)
+
+        layers.append(activation)
+        self.conv_block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.conv_block(x)
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, scale_factor=2, mode="bilinear", *args, **kwargs):
+        super(UpsampleBlock, self).__init__()
+        if mode != "nearest":
+            self.upsample_layer = nn.Upsample(scale_factor=scale_factor, 
+                                              mode=mode, align_corners=True)
+        else:
+            self.upsample_layer = nn.Upsample(scale_factor=scale_factor,
+                                              mode=mode)
+            
+    def forward(self, x):
+        return self.upsample_layer(x)
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super(UpBlock, self).__init__()
+        
+        activation = kwargs.get("activation") or nn.LeakyReLU(0.2)
+        self.upsample = UpsampleBlock()
+        self.conv_layer = ConvBlock(in_channels, out_channels, 
+                                    activation=activation)
+
+    def forward(self, down_conv, last_conv):
+        last_conv = self.upsample(last_conv)
+        x = torch.cat((last_conv, down_conv), dim=1)
+        return self.conv_layer(x)
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels, init_filters, depth, **kwargs):
+        super(Encoder, self).__init__()
+        layers = []
+        pool_size = kwargs.get("pool_size") or 2
+        pool_stride = kwargs.get("pool_stride") or 2
+
+        bn = kwargs.get("bn") or 0
+
+        jump = kwargs.get("jump") or 1
+
+        init_layer = ConvBlock(in_channels, init_filters)
+        layers.append(init_layer)
+
+        current_filters = init_filters
+        
+        for _ in range(depth - 1):
+            # Pooling Layer
+
+            for _ in range(jump - 1):
+                # Convolution Block
+                layer = ConvBlock(current_filters,
+                                  current_filters * 2, bn=bn)
+                layers.append(layer)
+
+                current_filters *= 2
+
+            # Pooling Block
+            layer = nn.MaxPool2d(kernel_size=pool_size, stride=pool_stride)
+            layers.append(layer)
+
+            # Convolution Block
+            layer = ConvBlock(current_filters,
+                              current_filters * 2, bn=bn)
+            layers.append(layer)
+
+            current_filters *= 2
+
+        self.encoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+class Decoder(nn.Module):
+    def __init__(self, init_filters, out_channels, depth, **kwargs):
+        super(Decoder, self).__init__()
+        layers = []
+
+        scale_factor = kwargs.get("scale_factor") or 2 
+        mode = kwargs.get("mode") or "bilinear"
+
+        bn = kwargs.get("bn") or 0
+
+        jump = kwargs.get("jump") or 1
+
+        current_filters = init_filters
+
+        for _ in range(depth - 1):    
+            for _ in range(jump - 1):
+                layer = ConvBlock(current_filters,
+                                  current_filters // 2, bn=bn)
+                layers.append(layer)
+
+                current_filters //= 2
+
+            layer = UpsampleBlock(scale_factor=scale_factor, mode=mode)
+            layers.append(layer)
+            layer = ConvBlock(current_filters,
+                              current_filters // 2)
+            layers.append(layer)
+
+            current_filters //= 2
+
+        layer = ConvBlock(current_filters, out_channels, 
+                          activation=nn.Sigmoid(), bn=bn)
+        layers.append(layer)
+        self.decoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.decoder(x)
---
+++ ISIS_4825/ML/Taller_12/train_utils.py
@@ -0,0 +1,117 @@
+import copy
+
+from torch.nn import functional as F
+import torch
+
+from tqdm.auto import tqdm
+
+def jaccard(y_pred, y_true, dim=(2, 3), eps=1e-3):
+    inter = (y_pred * y_true).sum(dim=dim) + eps
+    union = y_pred.sum(dim=dim) + y_true.sum(dim=dim) + eps
+    union -= inter
+    IoU = inter/ union
+    loss = 1 - IoU
+    return loss.mean(), IoU.mean()
+
+def loss_func(y_pred, y_true, metric=jaccard):
+    bce = F.binary_cross_entropy(y_pred, y_true, 
+                                 reduction="sum")
+    loss, acc = metric(y_pred, y_true)
+    loss += bce
+
+    return loss, acc
+
+def batch_loss(criterion, y_pred, y_true, opt=None):
+    loss, acc = criterion(y_pred, y_true)
+
+    if opt is not None:
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+    return loss.item(), acc.item()
+
+def epoch_loss(model, criterion, dataloader, device, sanity_check=False, opt=None):
+    epoch_loss = 0.
+    epoch_acc = 0.
+    len_data = len(dataloader.dataset)
+
+    for X_batch, y_batch in tqdm(dataloader):
+        X_batch = X_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        y_pred = model(X_batch)
+
+        b_loss, b_acc = batch_loss(criterion, y_pred, y_batch, opt)
+        epoch_loss += b_loss
+
+        if b_acc is not None:
+            epoch_acc += b_acc
+        if sanity_check:
+            break
+
+    loss = epoch_loss / float(len_data)
+    acc = epoch_acc / float(len_data)
+    return loss, acc
+
+def get_lr(opt):
+    for param_group in opt.param_groups:
+        return param_group['lr']
+
+def train(model, epochs, criterion, opt, train_dl, val_dl, 
+          sanity_check, lr_scheduler, weights_dir, device, **kwargs):
+    loss_history = {
+        "train": [],
+        "val": []
+    }
+
+    acc_history = {
+        "train": [],
+        "val": []
+    }
+
+    best_model = copy.deepcopy(model.state_dict())
+    best_loss = kwargs.get("best_loss") or float("inf")
+    best_acc = kwargs.get("best_acc") or float("inf")
+
+    for epoch in tqdm(range(epochs)):
+        current_lr = get_lr(opt)
+
+        model.train()
+        train_loss, train_acc = epoch_loss(model, criterion, train_dl, device,
+                                           sanity_check, opt)
+        loss_history["train"].append(train_loss)
+        acc_history["train"].append(train_acc)
+
+        model.eval()
+
+        with torch.no_grad():
+            val_loss, val_acc = epoch_loss(model, criterion, val_dl, device,
+                                           sanity_check)
+        
+        loss_history["val"].append(val_loss)
+        acc_history["val"].append(val_acc)
+
+        if val_loss < best_loss or val_acc > best_acc:
+            best_loss = val_loss
+            best_acc = val_acc
+            best_model = copy.deepcopy(model.state_dict())
+            
+            torch.save(model.state_dict(), weights_dir)
+            print("Copied best model weights!")
+        
+        lr_scheduler.step(val_loss)
+
+        if current_lr != get_lr(opt):
+            print("Loading best model weights!")
+            model.load_state_dict(best_model)
+
+        print(f"Train Loss: {train_loss:.6f}, Accuracy: {100 * train_acc:2f}")
+        print(f"Val loss: {val_loss:.6f}, Accuracy: {100 * val_acc:.2f}")
+        print("-"*50)
+
+        if sanity_check:
+            break
+
+    model.load_state_dict(best_model)
+    return model, loss_history, acc_history
