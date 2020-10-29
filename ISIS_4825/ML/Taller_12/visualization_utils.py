843fb3b33f6a0c6fa7d7f76c5369ebdfe359b9f8
Juan Pablo <jp.cano@uniandes.edu.co>
2020-10-28 19:06:57 -0500

    Arreglos

--- ISIS_4825/ML/Taller_12/visualization_utils.py
+++ ISIS_4825/ML/Taller_12/visualization_utils.py
@@ -1,6 +1,7 @@
 from skimage.segmentation import mark_boundaries
 
-def get_labeled_image(img, label):
-    img_mask = mark_boundaries(img, label, outline_color=(0, 1, 0), 
-                               color=(1, 0, 0), mode="thick")
+def get_labeled_image(img, label, outline_color=(1, 0, 0), 
+                        color=(1, 0, 0)):
+    img_mask = mark_boundaries(img, label, outline_color=outline_color, 
+                               color=color, mode="thick")
     return img_mask
