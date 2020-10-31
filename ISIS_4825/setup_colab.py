65bbea2f8823531f5fb73db36c76f458aa436d80
Juan Pablo <jp.cano@uniandes.edu.co>
2020-10-31 18:00:51 -0500

    Cambios Finales

--- ISIS_4825/setup_colab.py
+++ ISIS_4825/setup_colab.py
@@ -41,6 +41,7 @@ def setup_workshop_10(filename: str="kaggle.json",
 
 def setup_workshop_12(download_dataset=True, pretrained=True):
     setup_general.setup_general()
+    os.system("pip install -q albumentations==0.5.0")
     torch_path = "ISIS_4825/ML/Taller_12/torch_utils.py"
     vis_path = "ISIS_4825/ML/Taller_12/visualization_utils.py"
     layers_path = "ISIS_4825/ML/Taller_12/layers.py"
@@ -49,12 +50,14 @@ def setup_workshop_12(download_dataset=True, pretrained=True):
     setup_general.download_github_content(layers_path, "utils/layers.py")
     setup_general.download_github_content(train_path, "utils/train_utils.py")
     setup_general.download_github_content(vis_path, "utils/visualization_utils.py")
+    print("Util Functions Downloaded Successfully")
     from utils import general as gen
     if download_dataset:
         train_id = "192V5FfehmbpN2wkl1apiygxSqW6EUmyP"
         test_id = "1--hE7Ucvlsjf-fwET4-JMuS-VAva7Vxq"
         gen.download_file_from_google_drive(train_id, "train_data.zip", size=212e3, zip=True)
         gen.download_file_from_google_drive(test_id, "test_data.zip", size=22e3, zip=True)
+        print("Dataset Downloaded Successfully")
     if pretrained:
         autoencoder_id = "1e0N4VKKKQNIQ-nhFSiTjzhfSXiDiYYLs"
         unet_id = "1Np_5_sd-TpvSROgPSnTQQEzyHTYGQZY6"
@@ -62,4 +65,5 @@ def setup_workshop_12(download_dataset=True, pretrained=True):
                                             dst="./models", size=49e3)
         gen.download_file_from_google_drive(unet_id, "unet.pt",
                                             dst="./models", size=22e3)
+        print("Pretrained Networks Downloaded Successfully")
     print("Workshop 12 Enabled Successfully")
