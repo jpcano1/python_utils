720932299bcf4e742353f60946b37adaa163757d
Juan Pablo <jp.cano@uniandes.edu.co>
2020-11-21 13:53:45 -0500

    Mejoras

--- ISIS_4825/setup_colab.py
+++ ISIS_4825/setup_colab.py
@@ -74,7 +74,7 @@ def setup_workshop_13(download_dataset=True, pretrained=True):
         gen.download_file_from_google_drive(test_id, "test_data.zip", size=22e3)
         print("Dataset Downloaded Successfully")
     if pretrained:
-        autoencoder_id = "14Cd2Y4ssfWcKsketV2I4tl4rYOpVwzxh"
+        autoencoder_id = "1Pguxz_nQO8Wc8Gwo3-6y8vYBxC4IbJmE"
         unet_id = "1Np_5_sd-TpvSROgPSnTQQEzyHTYGQZY6"
         gen.download_file_from_google_drive(autoencoder_id, "autoencoder.pt",
                                             dst="./models", size=49e3)
