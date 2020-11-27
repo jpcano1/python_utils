69e51d7f72a7e86d590ae4d55fadc7f3a97a910a
Juan Pablo <jp.cano@uniandes.edu.co>
2020-11-27 02:43:11 -0500

    R2Unet

--- pytorch_utils/unet_architectures/layers.py
+++ pytorch_utils/unet_architectures/layers.py
@@ -196,3 +196,91 @@ class RecurrentUpBlock(nn.Module):
         last_block = self.upsample(last_block)
         x = torch.cat((last_block, down_block), dim=1)
         return self.conv_block(x)
+
+"""
+Recurrent Residual Layers
+"""
+class RRDownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, jump=2,
+                 *args, **kwargs):
+        """
+        Initializer method
+        :param in_channels: The number of in channels.
+        :param out_channels: The number of out channels.
+        :param jump: The number of convolutions per block
+        :param args: Function arguments.
+        :param kwargs: Function keyword arguments.
+        """
+        super(RRDownBlock, self).__init__()
+
+        assert jump > 1, "Jump must be greater than one"
+        # Create the layers list
+        layers = []
+
+        # The initial layer of the jump loop
+        self.init_layer = ConvBlock(in_channels, out_channels, 
+                                    *args, **kwargs)
+
+        # Append convolutional blocks if
+        # jump is greater than 1
+        for _ in range(jump - 1):
+            layer = RecurrentConvBlock(out_channels, 
+                                       *args, **kwargs)
+            layers.append(layer)
+
+        self.recurrent_block = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        """
+        The forward method
+        :param x: The tensor to be forwarded
+        :return: The tensor forwarded to down block
+        """
+        x1 = self.init_layer(x)
+        x2 = self.recurrent_block(x1)
+        out = x1 + x2
+        return out
+
+class RRUpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, jump=2,
+                 *args, **kwargs):
+        """
+        Initializer method
+        :param in_channels: The number of in channels
+        :param out_channels: The number of out channels
+        :param args: Function arguments
+        :param kwargs: Function keyword arguments
+        """
+        super(RRUpBlock, self).__init__()
+
+        jump = kwargs.get("jump") or 1
+        layers = []
+
+        # The upsampling layer
+        self.upsample = UpsampleBlock(*args, **kwargs)
+
+        # The first convolutional layer
+        self.init_layer = ConvBlock(in_channels, out_channels, 
+                                    *args, **kwargs)
+
+        # Append convolutional blocks
+        # if jump is greater than 1
+        for _ in range(jump - 1):
+            layer = RecurrentConvBlock(out_channels, 
+                                       *args, **kwargs)
+            layers.append(layer)
+        self.recurrent_block = nn.Sequential(*layers)
+    
+    def forward(self, down_block, last_block):
+        """
+        The forward method
+        :param down_block: The tensor from the down block
+        :param last_block: The tensor from the last block
+        :return:
+        """
+        last_block = self.upsample(last_block)
+        x = torch.cat((last_block, down_block), dim=1)
+        x1 = self.init_layer(x)
+        x2 = self.recurrent_block(x1)
+        out = x1 + x2
+        return out
