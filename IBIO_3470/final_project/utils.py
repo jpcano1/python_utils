dd158805955ac6b7e56ae039a071d9b4e9ac58a1
Juan Pablo <jp.cano@uniandes.edu.co>
2020-12-13 20:42:02 -0500

    Funciones Auxiliares

---
+++ IBIO_3470/final_project/utils.py
@@ -0,0 +1,228 @@
+from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances
+from sklearn.metrics import recall_score, precision_score, f1_score, make_scorer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import (precision_score, recall_score)
+from sklearn.cluster import KMeans
+
+from tqdm.auto import tqdm
+
+import numpy as np
+import pandas as pd
+
+from skimage import io, color, morphology, feature
+import os
+
+def preprocesamiento(img, selem):
+    img = morphology.erosion(img, selem)
+    return img
+
+class DataGenerator:
+    def __init__(self, data_dirs, grayscale=True, preprocessing=False,
+                 selem=None):
+        self.data_dirs = data_dirs
+        self.grayscale = grayscale
+        self.preprocessing = preprocessing
+        self.selem = selem
+        self.label_map = {
+            "MildDemented": 0,
+            "ModerateDemented": 0,
+            "NonDemented": 1,
+            "VeryMildDemented": 2
+        }
+    def __len__(self):
+        return len(self.data_dirs)
+    def size(self):
+        return len(self)
+    def __getitem__(self, idx):
+        img_dir = self.data_dirs[idx]
+        if self.grayscale:
+            img = color.rgb2gray(io.imread(img_dir))
+        else:
+            img = io.imread(img_dir)
+        if self.preprocessing:
+            img = preprocesamiento(img, self.selem)
+        label = img_dir.split(os.path.sep)[-2]
+        label = self.label_map[label]
+        return img, label
+
+def intensity_histogram(image, bins):
+    return np.histogram(image,bins=bins, density=True)[0]
+
+def validation_histograms(train, val, bins):
+    final_data={
+        "Clasificador":[],
+        "Precisión":[],
+        "Cobertura":[],
+        "F-Medida":[]
+    }
+    features = []
+    labels = []
+    for X, y in train:
+        ft = intensity_histogram(X, bins)
+        features.append(ft)
+        labels.append(y)
+    forest_clf = RandomForestClassifier().fit(features, labels)
+    svm_clf = SVC().fit(features, labels)
+    features =[]
+    labels = []
+    for X, y in val:
+        ft = intensity_histogram(X, bins)
+        features.append(ft)
+        labels.append(y)
+    y_pred = forest_clf.predict(features)
+    final_data["Clasificador"].append("Random Forest")
+    final_data["Cobertura"].append(recall_score(labels, y_pred, 
+                                                average="weighted"))
+    final_data["Precisión"].append(precision_score(labels, y_pred,  
+                                                   average="weighted"))
+    final_data["F-Medida"].append(f1_score(labels, y_pred, 
+                                           average="weighted"))
+    y_pred = svm_clf.predict(features)
+    final_data["Clasificador"].append("Support Vector Machine")
+    final_data["Cobertura"].append(recall_score(labels, y_pred,
+                                                average="weighted"))
+    final_data["Precisión"].append(precision_score(labels, y_pred,
+                                                   average="weighted"))
+    final_data["F-Medida"].append(f1_score(labels, y_pred,
+                                           average="weighted"))
+    return final_data
+
+def generate_hog(generator, params):
+    hog = list()
+    labels = np.zeros(len(generator))
+    index = 0
+    for X, y in tqdm(generator):
+        hog.append(feature.hog(X, **params, multichannel=False))
+        labels[index] = y
+        index += 1
+    return np.array(hog), labels
+
+def hog_descriptor(train_generator, val_generator, params, rf=True):
+    results = {
+        "Pixeles por Celda": [], "Orientaciones": [], 
+        "Clasificador": [], "Precisión": [], 
+        "Cobertura": [], "F-Medida": []
+    }
+
+    for param in tqdm(params):
+        train_hog, train_labels = generate_hog(train_generator, param)
+
+        if rf:
+            clf = RandomForestClassifier().fit(train_hog, train_labels)
+            print("RF Entrenado")
+            results["Clasificador"].append("Random Forest")
+
+        else:
+            clf = SVC().fit(train_hog, train_labels)
+            print("SVM Entrenado")
+            results["Clasificador"].append("Support Vector Machine")
+        
+        del train_hog, train_labels
+
+        val_hog, val_labels = generate_hog(val_generator, param)
+        y_pred = clf.predict(val_hog)
+        print("Predicciones hechas")
+
+        results["Pixeles por Celda"].append(param["pixels_per_cell"])
+        results["Orientaciones"].append(param["orientations"])
+
+        results["Cobertura"].append(recall_score(val_labels, y_pred, average="weighted"))
+        results["Precisión"].append(precision_score(val_labels, y_pred, average="weighted"))
+        results["F-Medida"].append(f1_score(val_labels, y_pred, average="weighted"))        
+        
+        del clf, y_pred
+        del val_hog, val_labels
+    return pd.DataFrame(results)
+
+def train_descriptor(train_data, train_labels, val_data, val_labels, 
+                     params=None, descriptor_name=None, 
+                     estimator=None):
+    results = {
+        "N Estimadores": [], "Criterio": [],
+        "Precisión": [], "Cobertura": [],
+        "F-medida": [], "Descriptor": []
+    }
+    for param in tqdm(params):
+        clf = estimator(**param)
+        results["N Estimadores"].append(param["n_estimators"])
+        results["Criterio"].append(param["criterion"])
+        clf.fit(train_data, train_labels)
+
+        y_pred = clf.predict(val_data)
+
+        results["Precisión"].append(precision_score(val_labels,y_pred, average="weighted"))
+        results["Cobertura"].append(recall_score(val_labels, y_pred, average="weighted"))
+        results["F-medida"].append(f1_score(val_labels, y_pred, average="weighted"))
+        results["Descriptor"].append(descriptor_name)
+
+        del y_pred, clf
+    return pd.DataFrame(results)
+
+def train_descriptor_svm(train_data, train_labels, val_data, val_labels, 
+                     params=None, descriptor_name=None, 
+                     estimator=None):
+    results = {
+        "Kernel": [], "C": [],
+        "Precisión": [], "Cobertura": [],
+        "F-medida": [], "Descriptor": []
+    }
+    for param in tqdm(params):
+        clf = estimator(**param)
+        results["Kernel"].append(param["kernel"])
+        results["C"].append(param["C"])
+        clf.fit(train_data, train_labels)
+
+        y_pred = clf.predict(val_data)
+
+        results["Precisión"].append(precision_score(val_labels,y_pred, average="weighted"))
+        results["Cobertura"].append(recall_score(val_labels, y_pred, average="weighted"))
+        results["F-medida"].append(f1_score(val_labels, y_pred, average="weighted"))
+        results["Descriptor"].append(descriptor_name)
+
+        del y_pred, clf
+    return pd.DataFrame(results)
+
+def lbp(img, p, r):
+    im = feature.local_binary_pattern(img, p, r)
+    n_bins = int(im.max() + 1)
+    return np.histogram(im, bins=n_bins, density=True, range=(0, n_bins))[0]
+
+def validation_lbp(train, val, p, r):
+    final_data={
+        "Clasificador":[],
+        "Precisión":[],
+        "Cobertura":[],
+        "F-Medida":[]
+    }
+    features = []
+    labels = []
+    for X, y in tqdm(train):
+        ft = lbp(X, p, r)
+        features.append(ft)
+        labels.append(y)
+    forest_clf = RandomForestClassifier().fit(features, labels)
+    svm_clf = SVC().fit(features, labels)
+    features=[]
+    labels=[]
+    for X, y in tqdm(val):
+        ft = lbp(X, p, r)
+        features.append(ft)
+        labels.append(y)
+    y_pred = forest_clf.predict(features)
+    final_data["Clasificador"].append("Random Forest")
+    final_data["Cobertura"].append(recall_score(labels, y_pred, 
+                                                average="weighted"))
+    final_data["Precisión"].append(precision_score(labels, y_pred,  
+                                                   average="weighted"))
+    final_data["F-Medida"].append(f1_score(labels, y_pred, 
+                                           average="weighted"))
+    y_pred = svm_clf.predict(features)
+    final_data["Clasificador"].append("Support Vector Machine")
+    final_data["Cobertura"].append(recall_score(labels, y_pred,
+                                                average="weighted"))
+    final_data["Precisión"].append(precision_score(labels, y_pred,
+                                                   average="weighted"))
+    final_data["F-Medida"].append(f1_score(labels, y_pred,
+                                           average="weighted"))
+    return final_data
