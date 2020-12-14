b87a2c56d65808f2b10565575c852ef49159d045
Juan Pablo <jp.cano@uniandes.edu.co>
2020-12-13 21:07:29 -0500

    Config

--- IBIO_3470/setup_colab.py
+++ IBIO_3470/setup_colab.py
@@ -16,6 +16,8 @@ def setup_project(download_dataset=True):
     if download_dataset:
         from libs import general as gen
         data_id = "1VZ9MvahloAaejUZUci-q_CIHTzfFtzsK"
+        utils_url = "https://raw.githubusercontent.com/jpcano1/python_utils/main/IBIO_3470/final_project/utils.py"
         gen.download_file_from_google_drive(data_id, "data.zip", size=35.87e3)
+        gen.download_content(utils_url, "utils.py", "libs")
         print("Dataset Downloaded Successfully")
     print("Workshop Project Enabled Successfully")
