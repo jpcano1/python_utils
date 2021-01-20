d61a09872b3bd00bffae9bb122360be473e1f763
Juan Pablo <jp.cano@uniandes.edu.co>
2021-01-19 22:06:00 -0500

    Autoencoder installation pending

--- pytorch_utils/setup_colab.py
+++ pytorch_utils/setup_colab.py
@@ -1,7 +1,7 @@
 import setup_colab_general as setup_general
 import os
 
-def setup_architecture(architecture="unet"):
+def setup_unet(architecture="unet"):
     os.makedirs("train_utils", exist_ok=True)
     with open("train_utils/__init__.py", "wb") as f:
         f.close()
@@ -38,3 +38,28 @@ def setup_architecture(architecture="unet"):
         attention_path = "pytorch_utils/unet_architectures/attention_unet.py"
         setup_general.download_github_content(attention_path, "utils/attention_unet.py")
         print("Attention U-Net Enabled")
+
+def setup_autoencoder(architecture="autoencoder"):
+    os.makedirs("train_utils", exist_ok=True)
+    with open("train_utils/__init__.py", "wb") as f:
+        f.close()
+
+    setup_general.setup_general()
+    metrics_path = "pytorch_utils/train_utils/metrics.py"
+    train_loop_path = "pytorch_utils/train_utils/seg_train_loops.py"
+    layers_path = "pytorch_utils/autoencoder_architectures/layers.py"
+    general_layers_path = "pytorch_utils/general_layers.py"
+
+    setup_general.download_github_content(general_layers_path, 
+                                          "utils/general_layers.py")
+    setup_general.download_github_content(metrics_path,
+                                          "train_utils/metrics.py")
+    setup_general.download_github_content(train_loop_path, 
+                                          "train_utils/train_loop.py")
+    setup_general.download_github_content(layers_path, "utils/layers.py")
+    print("Layers and utils enabled")
+
+    if architecture == "autoencoder" or "autoencoder" in architecture:
+        autoencoder_path = "pytorch_utils/autoencoder_architectures/autoencoder.py"
+        setup_general.download_github_content(autoencoder_path, "utils/autoencoder.py")
+        print("Autoencoder Enabled")
